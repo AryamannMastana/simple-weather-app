7f8d019967a1420667eb30aa78d8e596ec5ffc0a
Aryamann <aryamannmastana@gmail.com>
2022-08-05 21:00:58 +0530

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,29 @@
+//This JS file is for interacting witht the AccuWeather API
+
+const key = 'REkA7fEKVyjuo4pnshAEFD0jMXPTPqVb';
+
+// Get weather info
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+
+// Get city info
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    // Closest match is first element
+    return data[0];
+};
+
